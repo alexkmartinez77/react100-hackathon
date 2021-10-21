e9181d25205221366c9cd80cbe551c79437b3b5e
Alex Martinez <alexkmartinez77@gmail.com>
2021-10-21 16:01:47 -0700

    Adds chart to App.js and ChartIntro component.

---
+++ src/ChartIntro.jsx
@@ -0,0 +1,18 @@
+import React, {Component} from "react";
+
+class ChartIntro extends Component {
+
+  render() {
+    return (
+      <React.Fragment>
+        <p>Based on the data you provided, your calorie intake should be {this.props.caloricGoals} 
+            for your {this.props.goals} goals. The chart above will keep track of the calories you have
+            consumed {`(red bar)`} vs calories you have left {`(green bar)`} to eat for the day.
+        </p>
+        <p><a onclick={}>Start logging</a> your calories.</p>
+      </React.Fragment>
+    )
+  }
+}
+
+export default ChartIntro;
