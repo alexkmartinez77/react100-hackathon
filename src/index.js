ac24049bda8fc45f419999963aa87b9af968c955
Alex Martinez <alexkmartinez77@gmail.com>
2021-10-24 12:46:18 -0700

    Installs dotenv and requires dotenv in index.js

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './css/custom.scss';
+require('dotenv').config();
 
 ReactDOM.render(<App />, document.getElementById('root'));
