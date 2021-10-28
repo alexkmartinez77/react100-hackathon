5779850e2ab1eaec8ac16249778151ae513a0e05
Alex Martinez <alexkmartinez77@gmail.com>
2021-10-27 18:35:34 -0700

    Reorganizes incoming data to handle new food logs and new excercise logs. Log button is moved to display nutrition component.

--- src/DisplayFoodItemNutrition.jsx
+++ src/DisplayFoodItemNutrition.jsx
@@ -2,7 +2,7 @@ import React, {Component} from "react";
 
 class DisplayFoodItemNutrition extends Component {
   render() {
-    const {name, calories, protein, fat, carbohydrate} = this.props.foodItemData;
+    const {name, calories, protein, fat, carbohydrate} = this.props.caloriesInItem;
     return (
       <React.Fragment>
         <p>A {name} has {calories} calories and the following pofile :</p>
@@ -10,6 +10,7 @@ class DisplayFoodItemNutrition extends Component {
         <p>Fat: {fat}g</p>
         <p>Carbohydrates: {carbohydrate}g</p>
         <p>You can log this food item or calculate another.</p>
+        <button className="waves-effect waves-light btn" onClick={() => {this.props.logCaloriesIn(), this.props.closePage('needFoodData')}}>Log</button>
       </React.Fragment>
     )
   }
